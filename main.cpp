85d0b2ffc75137fa2eb3fdae69f331de16294986
Jason Palma <jasonpalma16@gmail.com>
2024-09-11 09:24:36 -0400

    Define class LinkedList

--- main.cpp
+++ main.cpp
@@ -7,6 +7,30 @@ struct node {
     node<Type>* next;
 };
 
+template <class Type>
+class LinkedList {
+private:
+    node<Type>* first;
+    int length;
+
+public:
+    void insertItem(Type);
+    void deleteItem(Type);
+    int getLength() const;
+    bool isEmpty() const;
+    bool searchItem(Type);
+    void printList() const;
+    void copy(const List<Type> &);
+    void destroy();
+    void print() const;
+    
+    LinkedList();
+    ~LinkedList();
+    LinkedList(const LinkedList<Type> &);
+    const LinkedList<Type> & operator=(const LinkedList<Type> &);
+
+};
+
 
 int main() {
 
